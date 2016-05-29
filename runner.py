5a26e09d8aa094ab86ae8b2a51ec1f6723126b25
Hung Nguyen Viet <hvn@familug.org>
2016-05-29 17:08:34 +0700

    add runner

---
+++ runner.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import logging
+import re
+import subprocess as spr
+
+logging.basicConfig()
+logger = logging.getLogger(__name__)
+
+success_pattern = re.compile('^Failed:\s.+0$')
+FORMULAS = ('vim', 'xml', 'virtualenv', 'pip')
+
+
+def main():
+    for formula in FORMULAS:
+        cmd = ['salt-call',
+               '-c', '/root/salt/states/test',
+               '-linfo',
+               'state.sls',
+               formula]
+
+        out = spr.check_output(cmd)
+        if success_pattern.findall(out):
+            logger.info("Test %s: successful.", formula)
+        else:
+            logger.info("Test %s: failed", formula)
+            logger.info(out)
+
+
+if __name__ == "__main__":
+    main()
